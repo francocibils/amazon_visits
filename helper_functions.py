3d8d92ed9c01017e28033126e631cc3c156fada1
francocibils <108623588+francocibils@users.noreply.github.com>
2024-10-17 10:55:53 -0300

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 def processing_visits(df, catalog, brand):
 
+    # Rename brand
+    brand = 'All ' + brand
+
     # Amazon dataframe
     amazon_df = pd.merge(df, catalog, left_on = 'SKU', right_on = 'SKU (AMAZON)', how = 'left')
     amazon_df = amazon_df[['Date', 'FAMILIA DE PRODUCTO', 'Sesiones - Total', 'Vistas de página - Total']]
@@ -13,7 +16,7 @@ def processing_visits(df, catalog, brand):
 
     # Total
     total_amazon_df = amazon_df.groupby('Date')[['Sessions', 'Page views']].sum()
-    total_amazon_df['Product family'] = 'All ' + brand
+    total_amazon_df['Product family'] = brand
     total_amazon_df = total_amazon_df.reset_index()
 
     # Group by date and family product
@@ -22,8 +25,18 @@ def processing_visits(df, catalog, brand):
     # Concat
     visits_df = pd.concat([amazon_df, total_amazon_df])
 
-    # Pivot
-    df_pivot = visits_df.pivot_table(index = 'Date', 
+    # Define all possible products and channels
+    all_days = visits_df['Date'].unique()
+    all_products = [brand] + catalog['FAMILIA DE PRODUCTO'].unique().tolist()
+
+    # Create a MultiIndex DataFrame with all combinations of products and channels
+    multi_index = pd.MultiIndex.from_product([all_days, all_products], names = ['Date', 'Product family'])
+
+    # Reindex the original DataFrame to this new index
+    df_reindexed = visits_df.set_index(['Date', 'Product family']).reindex(multi_index, fill_value = 0).reset_index()
+
+    # Pivot table
+    df_pivot = df_reindexed.pivot_table(index = 'Date', 
                           columns = 'Product family', 
                           values = ['Sessions', 'Page views'], 
                           aggfunc = 'sum')
@@ -33,7 +46,7 @@ def processing_visits(df, catalog, brand):
 
     # Order the columns as 'Product A - Sessions', 'Product A - Page views'
     sorted_columns = []
-    product_families = visits_df['Product family'].unique()
+    product_families = df_reindexed['Product family'].unique()
 
     for product in product_families:
         sorted_columns.append(f'{product} - Sessions')
@@ -42,6 +55,11 @@ def processing_visits(df, catalog, brand):
     # Reorder the columns
     df_pivot = df_pivot[sorted_columns]
 
+    # Reset index to bring 'date' back as a column
+    df_pivot = df_pivot.reset_index()
+    df_pivot = df_pivot.fillna(0)
+
+    return visits_df, df_pivot
     # Reset index to bring 'date' back as a column
     df_pivot = df_pivot.reset_index()
     df_pivot = df_pivot.fillna(0)
